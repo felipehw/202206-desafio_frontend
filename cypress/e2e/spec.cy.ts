3d8c86ac84f5a78fe4f4b5ce7363fed44b9d4c38
felipehw <felipehw@gmail.com>
2022-07-14 17:43:17 -0300

    Improved Cypress tests (plus cleaner style)

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,28 +1,37 @@
 const INITIAL_URL = 'http://localhost:3000/';
 
-describe('Index route render Users', () => {
+describe('Index route redirects to Users', () => {
   beforeEach(() => {
     cy.visit(INITIAL_URL);
   });
-  it('Visit site!', () => {
-    cy.get('body').find('[data-testid="Users"]').should('exist');
+  it('URL should be /users', () => {
+    cy.location("pathname")
+      .should("equal", "/users");
+  });
+  it('should render Users!', () => {
+    cy.get('[data-testid="Users"]')
+      .should('exist');
   });
 });
 
 describe('Acessing wrong path', () => {
-  it('Visit site, then go wrong path, redirected to 404!', () => {
-    cy.visit(INITIAL_URL);
-    cy.get('body').find('[data-testid="Users"]').should('exist');
-    cy.visit(INITIAL_URL + '/invalid');
-    cy.get('body').find('[data-testid="Page404"]').should('exist');
+  it('should render 404!', () => {
+    cy.visit(INITIAL_URL + '/invalid')
+      .get('[data-testid="Page404"]')
+      .should('exist');
   });
 });
 
 describe('Interacting in Users with menu & table', () => {
   it('Click in menu toggle, shows menu.', () => {
+    const dropdownMenuSelector = '.UsersMenu [data-testid="Dropdown.Menu"]';
+    const dropdownToggleSelector = '.UsersMenu [data-testid="Dropdown.Toggle"';
     cy.visit(INITIAL_URL);
-    cy.get('.UsersMenu').find('[data-testid="Dropdown.Menu"]').should('not.exist');
-    cy.get('[data-testid="Dropdown.Toggle"').click();
-    cy.get('.UsersMenu').find('[data-testid="Dropdown.Menu"]').should('exist');
+    cy.get(dropdownMenuSelector)
+      .should('not.exist');
+    cy.get(dropdownToggleSelector)
+      .click();
+    cy.get(dropdownMenuSelector)
+      .should('exist');
   });
 });
